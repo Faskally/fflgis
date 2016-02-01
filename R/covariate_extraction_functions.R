feae88d77b13a213baab045e08a558f707dc8e8c
Colin Millar <colinpmillar@googlemail.com>
2016-02-01 16:36:15 +0000

    fixed gradient calculation

--- R/covariate_extraction_functions.R
+++ R/covariate_extraction_functions.R
@@ -7,5 +7,5 @@ extractGradientFromLine <- function(sl, dtm) {
   p <- SpatialPoints(cc[c(1,nrow(cc)),], proj4string = crs(sl))
   el <- extract(dtm, p)
   # calculate gradient
-  (el[2] - el[1]) / SpatialLinesLengths(sl)
+  (el[1] - el[2]) / SpatialLinesLengths(sl)
 }
